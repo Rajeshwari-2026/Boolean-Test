0c90273615611f1b220b956d3d76dbe3722535fe
Rajeshwari <banothurajeshwari078@gmail.com>
2024-09-17 22:11:59 +0530

    Initial commit

---
+++ BooleanTest.java
@@ -0,0 +1,18 @@
+class BooleanTest{
+public static void main(String[] args){
+boolean b;
+b=false;
+System.out.println("b:"+b);
+b=true;
+System.out.println("b:"+b);
+if(b)
+System.out.println("this is executed");
+b=false;
+if(b)
+System.out.println("this is not executed");
+System.out.println("10>9 is"+(10>9));
+if(1)
+System.out.println("this is executed");}}
+
+
+}}
